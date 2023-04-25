0a8c0274661b6ad2c9f8579039772f100fff22b3
mercury_zhang <858482854@qq.com>
2023-04-25 10:56:16 +0800

    插槽

---
+++ vue3_demo/src/views/Dialog/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <header class="header">
+            <slot name="header"></slot>
+        </header>
+        <main class="main">
+            <div v-for="(item, index) in data" :key="index">
+                <slot name="main">{{ item.name }} -- {{ item.age }}</slot>
+            </div>
+        </main>
+        <footer class="footer">
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue"
+type names = {
+    name: string,
+    age: number
+}
+
+const data = reactive<names[]>([
+    {
+        name: '张三',
+        age: 18
+    },
+    {
+        name: '李四',
+        age: 17
+    },
+    {
+        name: '王五',
+        age: 20
+    },
+])
+</script>
+
+<style lang="scss" scoped></style>
