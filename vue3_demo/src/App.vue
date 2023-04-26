51f2404248e8491f5c8b25df622869a474ddaef7
mercury_zhang <858482854@qq.com>
2023-04-26 16:20:28 +0800

    nextTick

--- vue3_demo/src/App.vue
+++ vue3_demo/src/App.vue
@@ -1,18 +1,79 @@
 <template>
-    <div class="A">
-        <img id="img" width="300" height="600" src="./assets/images/1.jpg">
+    <div ref="box" class="wraps">
+        <div>
+            <div class="item" v-for="(item, index) in chatList" :key="index">
+                <div>{{ item.name }}:</div>
+                <div>{{ item.message }}:</div>
+            </div>
+        </div>
+    </div>
+    <div class="ipt">
+        <div>
+            <textarea v-model="ipt" type="text" />
+        </div>
+        <div>
+            <button @click="send">send</button>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import useBase64 from './hooks'
+import { nextTick, reactive, ref } from "vue"
+let chatList = reactive([
+    { name: '张三', message: "xxxxxxxxxxxx" }
+])
+let box = ref<HTMLDivElement>()
+let ipt = ref('')
+const send = async () => {
+    chatList.push({
+        name: 'zj',
+        message: ipt.value
+    })
+    await nextTick()
+    box.value!.scrollTop = 9999
+}
 
-useBase64({
-    el: '#img'
-}).then((res) => {
-    console.log(res.baseUrl)
-})
+</script>
 
-</script>   
+<style lang="scss" scoped>
+.wraps {
+    margin: 10px auto;
+    width: 500px;
+    height: 400px;
+    overflow: auto;
+    overflow-x: hidden;
+    background: #fff;
+    border: 1px solid #ccc;
 
-<style lang="scss" scoped></style>
+    .item {
+        width: 100%;
+        height: 50px;
+        background: #ccc;
+        display: flex;
+        align-items: center;
+        padding: 0 10px;
+        border-bottom: 1px solid #fff;
+    }
+}
+
+.ipt {
+    margin: 10px auto;
+    width: 500px;
+    height: 40px;
+    background: #fff;
+    border: 1px solid #ccc;
+
+    textarea {
+        width: 100%;
+        height: 100%;
+        border: none;
+        outline: none;
+    }
+
+    button {
+        width: 100px;
+        margin: 10px 0;
+        float: right;
+    }
+}
+</style>
---
+++ vue3_demo/src/AppHooks.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="A">
+        <img id="img" width="300" height="600" src="./assets/images/1.jpg">
+    </div>
+</template>
+
+<script setup lang="ts">
+import useBase64 from './hooks'
+
+useBase64({
+    el: '#img'
+}).then((res) => {
+    console.log(res.baseUrl)
+})
+
+</script>   
+
+<style lang="scss" scoped></style>
